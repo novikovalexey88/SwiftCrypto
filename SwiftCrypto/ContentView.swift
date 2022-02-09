4ab4b6c1a764303d98f17bf6ed13ac310ee71111
novikovalexey88 <novikov.a.m@yandex.ru>
2022-02-09 21:56:01 +0300

    commit2

---
+++ SwiftCrypto/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftCrypto
+//
+//  Created by 668 on 18.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Color.theme.background.ignoresSafeArea()
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftCrypto/SwiftCryptoApp.swift
@@ -0,0 +1,29 @@
+//
+//  SwiftCryptoApp.swift
+//  SwiftCrypto
+//
+//  Created by 668 on 18.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCryptoApp: App {
+    
+    @StateObject private var vm = HomeViewModel()
+    
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor(Color.theme.accent)]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor(Color.theme.accent)]
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                HomeView()
+                    .navigationBarHidden(true)
+            }
+            .environmentObject(vm)
+        }
+    }
+}
